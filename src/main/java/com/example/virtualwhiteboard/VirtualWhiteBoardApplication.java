2d8aecc0eca192e63dfc7e139c926a89a07d434f
Ficko05 <filip-f@hotmail.com>
2023-02-28 09:57:35 +0100

    create repo

---
+++ src/main/java/com/example/virtualwhiteboard/VirtualWhiteBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualwhiteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualWhiteBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualWhiteBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/virtualwhiteboard/VirtualWhiteBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualwhiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualWhiteBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
